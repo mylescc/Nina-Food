f222745bcb8355fd7f6b454c028f61c59eb40ea5
Myles Cowper-Coles <mylescc@gmail.com>
2013-12-03 10:45:37 +0000

    adding my name and adding tatler

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <div class="spacer"></div>
-<!--<div class="container">-->
-<!--    <div class="row">-->
-<!--        <div class="col-sm-12">-->
-<!--            <p class=" text-right">-->
-<!--            <a href="" class="myles-link">Made by @mcowpercoles</a>-->
-<!--            </p>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <p class=" text-right">
+            <a href="" class="myles-link">Made by @mcowpercoles</a>
+            </p>
+        </div>
+    </div>
+</div>
 
 
 <script src="js/jquery/jquery.min.js"></script>
--- press.php
+++ press.php
@@ -16,32 +16,32 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-4">
-<!--            <p>Style</p>-->
+            <a href="images/press/Nina_Tatler.pdf">
+                <img src="images/press/tatler-logo.png" alt="Nina's Tatler Article"/>
+            </a>
+        </div>
+        <div class="col-sm-4">
             <a href="images/press/STYLE.pdf">
                 <img src="images/press/style-logo.jpg" alt="Style"/>
             </a>
         </div>
         <div class="col-sm-4">
-<!--            <p>Evening Standard</p>-->
             <a href="images/press/Evening%20Standard%20press.pdf">
                 <img src="images/press/evening-standard-logo.jpg" alt="Evening Standard"/>
             </a>
         </div>
         <div class="col-sm-4">
-<!--            <p>Time out</p>-->
             <a href="images/press/Time%20out%20press.pdf">
                 <img src="images/press/timeout-logo.jpg" alt="Time Out"/>
             </a>
         </div>
 
         <div class="col-sm-4">
-<!--            <p>Vogue</p>-->
             <a href="images/press/Vogue%20Press.pdf">
                 <img src="images/press/vogue-logo.jpg" alt="Vogue"/>
             </a>
         </div>
         <div class="col-sm-4">
-<!--            <p>Elle</p>-->
             <a href="images/press/Elle%20press.pdf">
                 <img src="images/press/elle-logo.jpg" alt="Elle Press"/>
             </a>
