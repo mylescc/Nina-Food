6971dda6ee4cd35376fc75d396116204b8c7731b
Myles Cowper-Coles <mylescc@gmail.com>
2014-01-22 01:04:38 +0000

    added meta tag for google webmaster verification

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="description" content="NINA is a London based food company."/>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
+<meta name="google-site-verification" content="NvJqimuzPMW7RJio7XeBnOMZpR73w8aZq_FZTlnTPy8" />
     <link rel="stylesheet" href="css/bootstrap.min.css"/>
     <link rel="stylesheet" href="styles/main.css"/>
     <link rel="stylesheet" href="styles/home.css"/>
